517ed073cc2837ad92b3bc5f219f4a6ab35d9973
Rico Kandi <rico.kandi@voco.ee>
2026-09-10 13:55:11 +0300

    Täiendatud skript, tingimuslause tingimuslause sees

--- praks4/yl1.sh
+++ praks4/yl1.sh
@@ -1,12 +1,28 @@
 #!bin/bash
 #Tingimuslause ülesanne 1.
 
-read -p "Sisesta suvaline täisarv: " taisarv
 
 
-if (($taisarv % 2 == 0))
+#if (($taisarv % 2 == 0))
+#then
+#	echo Antud arv on paaris.
+#else
+#	echo Antud arv on paaritu.
+#fi
+
+
+
+#Täiendan skripti niimoodi, et panen tingimuslause tingimuslause sisse.
+
+#Kontrollin, kas käsurea parameetreid on 1
+if [ $# -ne 1 ]
 then
-	echo Antud arv on paaris.
+	echo "Käivita see skript koos arvuga mille paarsust soovid kontrollida. Näiteks: 6"
 else
-	echo Antud arv on paaritu.
+	if [ $(($1 % 2)) -eq 0 ]
+	then
+		echo "Antud arv on paaris"
+	else
+		echo "Antud arv on paaritu"
+	fi
 fi
