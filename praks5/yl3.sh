305d81a4c8799b06cf21969393c6e3a1a54aa0d5
Rico Kandi <rico.kandi@voco.ee>
2026-09-11 12:30:25 +0300

    Skripti muudetud niimoodi, et aja muutuja tuleb automaatselt ning tervitab kasutajat tema kasutajanime järgi

--- praks5/yl3.sh
+++ praks5/yl3.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 # Skript tervitab kasutajat vastavalt sisestatud kellaajale (tundides)
 
+aeg= $(date +%H)
+kasutaja=$USER
+
 read -p "Sisesta aja väärtus: " aeg
 
 if [ $aeg -ge 6 -a $aeg -lt 12 ]; then
-    echo "Tere hommikust!"
+    echo "Tere hommikust, $kasutaja !"
 elif [ $aeg -ge 12 -a $aeg -lt 18 ]; then
-    echo "Tere päevast!"
+    echo "Tere päevast, $kasutaja !"
 elif [ $aeg -ge 18 -a $aeg -lt 22 ]; then
-    echo "Tere õhtust!"
+    echo "Tere õhtust, $kasutaja !"
 elif [ $aeg -ge 22 -a $aeg -lt 24 ] || [ $aeg -ge 0 -a $aeg -lt 6 ]; then
-    echo "Head ööd!"
+    echo "Head ööd, $kasutaja !"
 else
     echo "Vigane kellaaeg! Sisesta tund vahemikus 0-23."
 fi
